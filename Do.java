b0e4f296c46802b990e91fc3f2ab93e505095a4a
Dhiraj kumar <149323248+Dhirajsingh12@users.noreply.github.com>
2025-02-15 00:30:30 +0530

    loop

---
+++ Do.java
@@ -0,0 +1,11 @@
+
+public class Do {
+
+    public static void main(String[] args) {
+        int num = 1;
+        do {
+            System.out.println(num);
+            num += 1;
+        } while (num <= 9);
+    }
+}
