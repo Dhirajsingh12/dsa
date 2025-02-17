e15aef30171eeb88c820f2cc43ff376d9fa4997b
Dhiraj kumar <149323248+Dhirajsingh12@users.noreply.github.com>
2025-02-18 00:29:20 +0530

    count

---
+++ Count.java
@@ -0,0 +1,18 @@
+
+public class Count {
+
+    public static void main(String[] args) {
+        int n = 2345555;
+        int count = 0;
+        while (n > 0) {
+            int rem = n % 10;
+            if (rem == 5) {
+                count++;
+
+            }
+            n = n / 10;
+
+        }
+        System.out.println(count);
+    }
+}
