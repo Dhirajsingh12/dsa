23d4d1544ec8fe856c5d0c893d76626a15e000ce
Dhiraj kumar <149323248+Dhirajsingh12@users.noreply.github.com>
2025-02-21 23:42:20 +0530

    multi array

---
+++ Multi.java
@@ -0,0 +1,10 @@
+
+public class Multi {
+
+    public static void main(String[] args) {
+        int[][] arr1 = new int[4][3];
+        int[][] arr = {{34, 45, 23, 89}, {32, 54, 32}, {45, 98, 7}};
+        System.out.println(arr[0][0]);
+
+    }
+}
