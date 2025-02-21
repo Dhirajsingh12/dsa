30dbac1a8bd02f598fd006ef2e66acd14fdd63a2
Dhiraj kumar <149323248+Dhirajsingh12@users.noreply.github.com>
2025-02-21 22:16:11 +0530

    array

---
+++ Ages.java
@@ -0,0 +1,14 @@
+
+public class Ages {
+
+    public static void main(String[] args) {
+
+        int age[] = new int[3];
+        age[0] = 1;
+        age[1] = 3;
+        age[2] = 4;
+        System.out.println(age[2]);
+        System.out.println(age[1]);
+        System.out.println(age[0]);
+    }
+}
