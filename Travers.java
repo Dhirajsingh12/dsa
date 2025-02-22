fdf7a6d45d94687755f6e7eb9d57f6e013f8fd84
Dhiraj kumar <149323248+Dhirajsingh12@users.noreply.github.com>
2025-02-22 23:08:11 +0530

    travers

---
+++ Travers.java
@@ -0,0 +1,16 @@
+
+public class Travers {
+
+    public static void main(String[] args) {
+        int[] arr = {2, 4, 3, 5};
+        for (int i = 0; i < arr.length; i++) {
+            if (i == 3) {
+                break;
+
+            }
+            System.out.println(arr[i]);
+
+        }
+
+    }
+}
