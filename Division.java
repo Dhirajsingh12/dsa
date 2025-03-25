7dfa27e5baae53be4c31c71798afc6c9f238641a
Dhiraj kumar <149323248+Dhirajsingh12@users.noreply.github.com>
2025-03-25 22:26:44 +0530

    java

--- Division.java
+++ Division.java
@@ -5,11 +5,11 @@ public class Division {
 
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        System.out.println("enter your frist number");
+        System.out.println("enter your frist number:-");
         int a = input.nextInt();
-        System.out.println("enter your second number");
+        System.out.println("enter your second number:-");
         int b = input.nextInt();
         int c = a / b;
-        System.out.println("your multiplication" + c);
+        System.out.println("your Division=" + c);
     }
 }
