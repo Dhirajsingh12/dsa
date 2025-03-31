a2f9c2974a184cb53cbd57b27ec0bc06752335c5
Dhiraj kumar <149323248+Dhirajsingh12@users.noreply.github.com>
2025-03-31 22:35:03 +0530

    java

---
+++ .vscode/intro.java
@@ -0,0 +1,17 @@
+
+public class intro {
+
+    int a = 10;
+
+    void run() {
+        System.out.println("i am good");
+
+    }
+
+    public static void main(String[] args) {
+        intro obj = new intro();
+        obj.run();
+        System.out.println(obj.a);
+    }
+
+}
