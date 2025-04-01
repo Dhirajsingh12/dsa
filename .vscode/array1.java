7cca57090e7a3af7175116071eb1e3f7c4e16444
Dhiraj kumar <149323248+Dhirajsingh12@users.noreply.github.com>
2025-04-01 23:22:07 +0530

    change

--- .vscode/array1.java
+++ .vscode/array1.java
@@ -3,6 +3,22 @@ public class array1 {
 
     void arraydemo() {
         int age[] = new int[3];
+        int weight[] = new int[4];
+        String name[] = new String[3];
+        name[0] = "dhiraj";
+        name[1] = "amit";
+        name[2] = "shubham";
+        System.out.println(name[0]);
+        System.out.println(name[1]);
+        System.out.println(name[2]);
+        weight[0] = 10;
+        weight[1] = 110;
+        weight[2] = 1011;
+        weight[3] = 101;
+        System.out.println(weight[0]);
+        System.out.println(weight[1]);
+        System.out.println(weight[2]);
+        System.out.println(weight[3]);
         age[0] = 12;
         age[1] = 23;
         age[2] = 34;
