8ee7b4026cf8a0b0196afca550f87ac45a93a1b4
Dhiraj kumar <149323248+Dhirajsingh12@users.noreply.github.com>
2025-04-02 10:09:33 +0530

    sum

---
+++ sum11.java
@@ -0,0 +1,10 @@
+
+public class sum11 {
+
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 12;
+        int sum = a + b;
+        System.out.println("sum of two number=" + sum);
+    }
+}
