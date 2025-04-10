3d5f892dc55aae48d07ed2684e77a99c938b8634
Dhiraj kumar <149323248+Dhirajsingh12@users.noreply.github.com>
2025-04-10 20:59:15 +0530

    sdf

--- min.java
+++ min.java
@@ -2,14 +2,14 @@
 public class min {
 
     public static void main(String[] args) {
-        int[] numbers = {45, 12, 78, 4, 89, 1, 22};
+        int[] numbers = {45, 12, 78, 4, 89, 10, 45, 22, 67};
 
         int min = numbers[0];
 
         for (int i = 1; i < numbers.length; i++) {
             if (numbers[i] < min) {
                 min = numbers[i];
-            
+
             }
         }
 
