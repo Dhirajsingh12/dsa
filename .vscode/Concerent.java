dbd5c25a776008b651d697fce49225346eee3a83
Dhiraj kumar <149323248+Dhirajsingh12@users.noreply.github.com>
2025-05-23 01:52:24 +0530

    dsa

---
+++ .vscode/Concerent.java
@@ -0,0 +1,12 @@
+
+public class Concerent {
+
+    public static void main(String[] args) {
+        double price = 100.00;
+        double finalprice = price + 18;
+        System.out.println("finalprice =" + finalprice);
+        int p = 100;
+        int fp = p + (int) 18.01;
+        System.out.println(fp);
+    }
+}
---
+++ .vscode/User.java
@@ -0,0 +1,12 @@
+
+import java.util.Scanner;
+
+public class User {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter your name");
+        String name = sc.nextLine();
+        System.err.println(name);
+    }
+}
