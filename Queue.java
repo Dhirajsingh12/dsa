73c4fe35e289926f3cd45cf38e0a7a78b9a6eb8b
Dhiraj kumar <149323248+Dhirajsingh12@users.noreply.github.com>
2025-06-24 20:05:58 +0530

    linked list

---
+++ Queue.java
@@ -0,0 +1,17 @@
+
+import java.util.*;
+
+public class Queue {
+
+    static void queuelist() {
+        LinkedList<Integer> q = new LinkedList<>();
+        q.offer(1);
+        q.offer(2);
+        q.offer(3);
+        System.out.println(q);
+    }
+
+    public static void main(String[] args) {
+        queuelist();
+    }
+}
